13ecc4397c8dd10dc6306c1496da2299b54b73da
RizalKubro <127368706+RizalKubro@users.noreply.github.com>
2023-05-25 09:31:36 +0700

    Add files via upload

---
+++ main.java
@@ -0,0 +1,23 @@
+
+package node;
+
+public class main {
+    
+    public static void main(String[] args) {
+        BinartTree pohon = new BinartTree();
+
+        pohon.NewNode(20);
+        pohon.NewNode(2);
+        pohon.NewNode(25);
+        pohon.NewNode(37);
+        pohon.NewNode(16);
+
+        System.out.print("\nPre Order : ");
+        pohon.preOrder(pohon.root);
+        System.out.print("\nIn Order : ");
+        pohon.inOrder(pohon.root);
+        System.out.print("\nPost Order : ");
+        pohon.postOrder(pohon.root);
+    }
+}
+
